07eb243d39dd9888e38f988b3b0b72393fbf9ae9
Manish <manishkr117@gmail.com>
2026-06-17 23:30:44 +0530

    Added AnonymousDelegate

---
+++ Practicing_C_Sharp/AnonymousDelegate/AnonymousDelegate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousDelegate
+{
+    internal class Program
+    {
+
+        public delegate void Calculator(int a, int b);
+        static void Main(string[] args)
+        {
+            Calculator add = delegate (int a, int b)
+            {
+                Console.WriteLine($"Addition: {a + b}");
+            };
+            add(5, 6);
+        }
+    }
+}
