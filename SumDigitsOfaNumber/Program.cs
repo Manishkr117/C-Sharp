c8acb5e968a4ffb9fdf9555b4d65381692562092
Manish <manishkr117@gmail.com>
2026-06-27 21:07:25 +0530

    Added Sum of digits of a number

---
+++ SumDigitsOfaNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumDigitsOfaNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number to calculate the sum of its digits:");
+            int number = Convert.ToInt32(Console.ReadLine());
+           int sum = CalculateSumOfDigits(number);
+            Console.WriteLine($"The sum of the digits of {number} is {sum}"); Console.WriteLine(sum);
+        }
+
+        static int CalculateSumOfDigits(int number)
+        {
+            int sum = 0;
+            while (number != 0)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+            return sum;
+        }
+    }
+}
